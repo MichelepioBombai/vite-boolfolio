636715f111f1b9a01f3e0d30c6163a62fcff0468
MichelepioBombai <michelebombai@icloud.com>
2023-04-27 16:16:09 +0200

    add navbar

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import AppHeader from "./components/AppHeader.vue";
+
+export default {
+  data() {
+    return {};
+  },
+
+  components: { AppHeader },
+};
+</script>
+
+<template>
+  <AppHeader />
+  <h1>hello world</h1>
+</template>
+
+<style lang="scss"></style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  name: "AppHeader",
+};
+</script>
+
+<template>
+  <nav class="navbar bg-body-tertiary">
+    <div class="container">
+      <a class="navbar-brand" href="#">Vue Boolpress</a>
+    </div>
+  </nav>
+</template>
+
+<style lang="scss"></style>
