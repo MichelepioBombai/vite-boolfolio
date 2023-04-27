375b25ce44597f7f0a7c420dfbe8fb864b845e2c
MichelepioBombai <michelebombai@icloud.com>
2023-04-27 16:47:33 +0200

    add title in Html

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,24 @@
 <script>
+import axios from "axios";
 import AppHeader from "./components/AppHeader.vue";
 
 export default {
   data() {
-    return {};
+    return {
+      projects: [],
+    };
+  },
+
+  methods: {
+    fetchProjects() {
+      axios.get("http://127.0.0.1:8000/api/projects").then((response) => {
+        this.projects = response.data;
+      });
+    },
+  },
+
+  created() {
+    this.fetchProjects();
   },
 
   components: { AppHeader },
@@ -13,6 +28,9 @@ export default {
 <template>
   <AppHeader />
   <h1>hello world</h1>
+  <ul>
+    <li v-for="project in projects">{{ project.title }}</li>
+  </ul>
 </template>
 
 <style lang="scss"></style>
